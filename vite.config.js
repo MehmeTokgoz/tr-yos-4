e64cf6bb2b670201ae0d9fe7840a1aa5f57e69ea
MEHMETTOKGOZ <42mt06@gmail.com>
2023-08-07 10:21:48 +0300

    vite config and register api changed

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,44 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// // eslint-disable-next-line no-unused-vars
+// export default defineConfig(({ command, mode }) => {
+//   const isProduction = mode === "production";
+
+//   // Vite Proxy configuration
+//   const detailProxyOptions = {
+//     "/api": {
+//       target: "https://tr-yös.com",
+//       // port: 5173,
+//       changeOrigin: true,
+//       // rewrite: (path) => path.replace(/^\/api/, ""),
+//     },
+//   };
+
+//   // Different configuration for production and development mode
+//   return {
+//     plugins: [react()],
+//     server: {
+//       proxy: isProduction
+//         ? {}
+//         : { ...detailProxyOptions},
+//     },
+//   };
+// });
+
+
+
+
+
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// eslint-disable-next-line no-unused-vars
-export default defineConfig(({ command, mode }) => {
-  const isProduction = mode === "production";
 
-  // Vite Proxy configuration
-  const detailProxyOptions = {
-    "/api": {
-      target: "https://tr-yös.com",
-      // port: 5173,
-      changeOrigin: true,
-      // rewrite: (path) => path.replace(/^\/api/, ""),
-    },
-  };
-
-  // Different configuration for production and development mode
-  return {
-    plugins: [react()],
-    server: {
-      proxy: isProduction
-        ? {}
-        : { ...detailProxyOptions},
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
-
-
-
-
-
-
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-
-
-
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
